474222ff9784c179f34b07f423aa2c2a90b899a5
Bockrr <67198413+Bockrr@users.noreply.github.com>
2024-02-19 22:32:39 -0500

    removed unused test files, made some progress on patient form. Started components folder.

--- health-app/src/App.js
+++ health-app/src/App.js
@@ -1,13 +1,31 @@
-import './App.css';
-
+import { useState }from 'react';
+import RegistrationForm from './components/RegistrationForm';
 
 function App() {
+  const[username, setUsername] = useState('');
+  const[password, setPassword] = useState('');
+//event handler for user name input change
+  const handleUsernameInput = (event) => {
+    console.log(event.target.value)
+    setUsername(event.target.value)
+  }
+  //event handler for password input change
+  const handlePasswordInput = (event) => {
+    console.log(event.target.value)
+    setPassword(event.target.value)
+
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+//TO DO: REMOVE RETURN AND ENTER backend logic
+    return(
+    console.log(username), 
+    console.log(password)
+    )
+  }
   return (
-    <div className="App">
-        <p>
-          Health App
-        </p>
-    </div>
+    <RegistrationForm handlePasswordInput={handlePasswordInput} handleSubmit={handleSubmit} handleUsernameInput={handleUsernameInput} />
   );
 }
 
