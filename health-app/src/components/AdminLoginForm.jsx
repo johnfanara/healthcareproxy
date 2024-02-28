66206c87de9210d7919e7270cbea59a0513e2b97
johnfanara <64872359+johnfanara@users.noreply.github.com>
2024-02-27 23:21:36 -0500

    Revert "enabled firebase login"

--- health-app/src/components/AdminLoginForm.jsx
+++ health-app/src/components/AdminLoginForm.jsx
@@ -1,57 +1,16 @@
-import React, {useState} from 'react';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../firebase';
+import React from 'react';
 
-const AdminLoginForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const onLogin = (e) => {
-    e.preventDefault();
-    signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      console.log(user);
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorCode, errorMessage)
-    });
-  }
-
+const AdminLoginForm = ({ handleEmailInput, handlePasswordInput, handleSubmit }) => {
   return (
     <div className="LoginForm">
       <h2>Admin Login</h2>
-      <form>
-        <div>
-          <input
-              id="email-address"
-              name="email"
-              type="email"
-              required
-              placeholder="Email address"
-              onChange={(e)=>setEmail(e.target.value)}
-          />
-        </div>
-
-        <div>
-            <input
-                id="password"
-                name="password"
-                type="password"
-                required
-                placeholder="Password"
-                onChange={(e)=>setPassword(e.target.value)} 
-            />
-        </div>
-
-        <div>
-            <button onClick={onLogin}>Login</button>        
-        </div>
+      <form className="initialFormInput">
+        <input type="text" name="email" placeholder="Enter email" onChange={handleEmailInput} />
+        <input type="text" name="password" placeholder="Enter password" onChange={handlePasswordInput} />
       </form>
+      <button onClick={handleSubmit}>Login</button>
     </div>
-  )
-}
+  );
+};
 
-export default AdminLoginForm
+export default AdminLoginForm;
