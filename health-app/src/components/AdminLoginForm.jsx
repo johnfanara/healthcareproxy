2ef4f20f4c4196dafdb8c75ec3c5c1340e64fce5
johnf <johnfanara1138@gmail.com>
2024-03-06 15:30:02 -0500

    enabled password reset, updated error messages

--- health-app/src/components/AdminLoginForm.jsx
+++ health-app/src/components/AdminLoginForm.jsx
@@ -1,12 +1,34 @@
 import React, { useState } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import { sendPasswordResetEmail } from 'firebase/auth';
 import { auth } from '../App';
 
 const AdminLoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
+  const [forgotPassword, setForgotPassword] = useState(false);
 
+  const onResetPassword = async (e) => {
+    e.preventDefault();
+
+    try {
+      await sendPasswordResetEmail(auth, email);
+      setMessage("Password reset email sent. Check your inbox.");
+      setForgotPassword(false);
+
+      setTimeout(() => {
+        setMessage('');
+      }, 3000);
+    } catch (error) {
+      console.error("Forgot password error: ", error.message);
+      setMessage("Error: Unable to send reset email. Please try again.");
+      
+      setTimeout(() => {
+        setMessage('');
+      }, 3000);
+    }
+  }
   const onSubmit = async (e) => {
     e.preventDefault();
 
@@ -17,8 +39,9 @@ const AdminLoginForm = () => {
 
       setEmail('');
       setPassword('');
-      
+
       setMessage('Login successful!');
+      setForgotPassword(false);
 
       setTimeout(() => {
         setMessage('');
@@ -26,7 +49,18 @@ const AdminLoginForm = () => {
     } catch(error) {
       console.error("Admin login error: ", error.message);
 
-      setMessage('Error: ${error.message}');
+      setForgotPassword(true);
+      let errorMessage = "Login failed. Please check your email and password.";
+
+      switch (error.code) {
+        case "auth/invalid-email":
+          errorMessage = "Invalid email address.";
+          break;
+        default:
+          break;
+      }
+
+      setMessage(errorMessage);
 
       setTimeout(() => {
         setMessage('');
@@ -57,6 +91,24 @@ const AdminLoginForm = () => {
         </button>
         {message && <p className="message">{message}</p>}
       </form>
+
+      {forgotPassword && (
+        <div>
+          <p>Forgot your password?</p>
+          <form className="initialFormInput">
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button type="submit" onClick={onResetPassword}>
+              Reset Password
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   )
 }
